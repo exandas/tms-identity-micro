129cdbd7eff8f8fa707834e232b349c6d4fbec74
Kostas Mamalis <kostas@masterthought.net>
2015-07-09 15:09:27 +0100

    first commit for user service

--- src/main/scala/AkkaHttpMicroservice.scala
+++ src/main/scala/AkkaHttpMicroservice.scala
@@ -17,40 +17,15 @@ import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.math._
 import spray.json.DefaultJsonProtocol
 
-case class IpInfo(ip: String, country: Option[String], city: Option[String], latitude: Option[Double], longitude: Option[Double])
+case class UserLoginInfo(username: String, password: String)
 
-case class IpPairSummaryRequest(ip1: String, ip2: String)
 
-case class IpPairSummary(distance: Option[Double], ip1Info: IpInfo, ip2Info: IpInfo)
 
-object IpPairSummary {
-  def apply(ip1Info: IpInfo, ip2Info: IpInfo): IpPairSummary = IpPairSummary(calculateDistance(ip1Info, ip2Info), ip1Info, ip2Info)
-
-  private def calculateDistance(ip1Info: IpInfo, ip2Info: IpInfo): Option[Double] = {
-    (ip1Info.latitude, ip1Info.longitude, ip2Info.latitude, ip2Info.longitude) match {
-      case (Some(lat1), Some(lon1), Some(lat2), Some(lon2)) =>
-        // see http://www.movable-type.co.uk/scripts/latlong.html
-        val φ1 = toRadians(lat1)
-        val φ2 = toRadians(lat2)
-        val Δφ = toRadians(lat2 - lat1)
-        val Δλ = toRadians(lon2 - lon1)
-        val a = pow(sin(Δφ / 2), 2) + cos(φ1) * cos(φ2) * pow(sin(Δλ / 2), 2)
-        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        Option(EarthRadius * c)
-      case _ => None
-    }
-  }
-
-  private val EarthRadius = 6371.0
+trait UserProtocols extends DefaultJsonProtocol {
+  implicit val userFormat = jsonFormat2(UserLoginInfo.apply)
 }
 
-trait Protocols extends DefaultJsonProtocol {
-  implicit val ipInfoFormat = jsonFormat5(IpInfo.apply)
-  implicit val ipPairSummaryRequestFormat = jsonFormat2(IpPairSummaryRequest.apply)
-  implicit val ipPairSummaryFormat = jsonFormat3(IpPairSummary.apply)
-}
-
-trait Service extends Protocols {
+trait Service extends UserProtocols {
   implicit val system: ActorSystem
   implicit def executor: ExecutionContextExecutor
   implicit val materializer: Materializer
@@ -58,44 +33,25 @@ trait Service extends Protocols {
   def config: Config
   val logger: LoggingAdapter
 
-  lazy val telizeConnectionFlow: Flow[HttpRequest, HttpResponse, Any] =
-    Http().outgoingConnection(config.getString("services.telizeHost"), config.getInt("services.telizePort"))
 
-  def telizeRequest(request: HttpRequest): Future[HttpResponse] = Source.single(request).via(telizeConnectionFlow).runWith(Sink.head)
-
-  def fetchIpInfo(ip: String): Future[Either[String, IpInfo]] = {
-    telizeRequest(RequestBuilding.Get(s"/geoip/$ip")).flatMap { response =>
-      response.status match {
-        case OK => Unmarshal(response.entity).to[IpInfo].map(Right(_))
-        case BadRequest => Future.successful(Left(s"$ip: incorrect IP format"))
-        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
-          val error = s"Telize request failed with status code ${response.status} and entity $entity"
-          logger.error(error)
-          Future.failed(new IOException(error))
-        }
+  def fetchUser(username: String): Future[Either[String, UserLoginInfo]] = {
+    Future {
+      if(username == null){
+        Left("wrong username")
+      } else {
+        Right(UserLoginInfo(username,"temp"))
       }
     }
   }
 
   val routes = {
     logRequestResult("akka-http-microservice") {
-      pathPrefix("ip") {
-        (get & path(Segment)) { ip =>
+      pathPrefix("user") {
+        (get & path(Segment)) { user =>
           complete {
-            fetchIpInfo(ip).map[ToResponseMarshallable] {
+            fetchUser(user).map[ToResponseMarshallable] {
               case Right(ipInfo) => ipInfo
               case Left(errorMessage) => BadRequest -> errorMessage
-            }
-          }
-        } ~
-        (post & entity(as[IpPairSummaryRequest])) { ipPairSummaryRequest =>
-          complete {
-            val ip1InfoFuture = fetchIpInfo(ipPairSummaryRequest.ip1)
-            val ip2InfoFuture = fetchIpInfo(ipPairSummaryRequest.ip2)
-            ip1InfoFuture.zip(ip2InfoFuture).map[ToResponseMarshallable] {
-              case (Right(info1), Right(info2)) => IpPairSummary(info1, info2)
-              case (Left(errorMessage), _) => BadRequest -> errorMessage
-              case (_, Left(errorMessage)) => BadRequest -> errorMessage
             }
           }
         }
--- src/test/scala/ServiceSpec.scala
+++ src/test/scala/ServiceSpec.scala
@@ -12,55 +12,23 @@ class ServiceSpec extends FlatSpec with Matchers with ScalatestRouteTest with Se
   override def config = testConfig
   override val logger = NoLogging
 
-  val ip1Info = IpInfo("8.8.8.8", Option("United States"), Option("Mountain View"), Option(37.386), Option(-122.0838))
-  val ip2Info = IpInfo("8.8.4.4", Option("United States"), None, Option(38.0), Option(-97.0))
-  val ipPairSummary = IpPairSummary(ip1Info, ip2Info)
+  val userInfo = UserLoginInfo("kostas","temp")
+  val userInfo2 = UserLoginInfo("jim","temp")
 
-  override lazy val telizeConnectionFlow = Flow[HttpRequest].map { request =>
-    if (request.uri.toString().endsWith(ip1Info.ip))
-      HttpResponse(status = OK, entity = marshal(ip1Info))
-    else if(request.uri.toString().endsWith(ip2Info.ip))
-      HttpResponse(status = OK, entity = marshal(ip2Info))
-    else
-      HttpResponse(status = BadRequest, entity = marshal("Bad ip format"))
-  }
 
-  "Service" should "respond to single IP query" in {
-    Get(s"/ip/${ip1Info.ip}") ~> routes ~> check {
+  "Service" should "respond to a single user query" in {
+    Get(s"/user/${userInfo.username}") ~> routes ~> check {
       status shouldBe OK
       contentType shouldBe `application/json`
-      responseAs[IpInfo] shouldBe ip1Info
+      responseAs[UserLoginInfo] shouldBe userInfo
     }
 
-    Get(s"/ip/${ip2Info.ip}") ~> routes ~> check {
+    Get(s"/user/${userInfo2.username}") ~> routes ~> check {
       status shouldBe OK
       contentType shouldBe `application/json`
-      responseAs[IpInfo] shouldBe ip2Info
+      responseAs[UserLoginInfo] shouldBe userInfo2
     }
   }
 
-  it should "respond to IP pair query" in {
-    Post(s"/ip", IpPairSummaryRequest(ip1Info.ip, ip2Info.ip)) ~> routes ~> check {
-      status shouldBe OK
-      contentType shouldBe `application/json`
-      responseAs[IpPairSummary] shouldBe ipPairSummary
-    }
-  }
 
-  it should "respond with bad request on incorrect IP format" in {
-    Get("/ip/asdfg") ~> routes ~> check {
-      status shouldBe BadRequest
-      responseAs[String].length should be > 0
-    }
-
-    Post(s"/ip", IpPairSummaryRequest(ip1Info.ip, "asdfg")) ~> routes ~> check {
-      status shouldBe BadRequest
-      responseAs[String].length should be > 0
-    }
-
-    Post(s"/ip", IpPairSummaryRequest("asdfg", ip1Info.ip)) ~> routes ~> check {
-      status shouldBe BadRequest
-      responseAs[String].length should be > 0
-    }
-  }
 }
